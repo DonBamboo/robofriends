340e44f882a0d403fb8f6f27cf967886c93e5851
unknown <jpearlz0123@gmail.com>
2022-01-10 22:15:54 +0800

    deleted scroll.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
-import Scroll from '../components/Scroll';
 import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
