b7706468c665c1fdf151a38eff816a28f9d086fd
unknown <jpearlz0123@gmail.com>
2022-01-12 01:55:09 +0800

    redux middleware working

--- src/reducers.js
+++ src/reducers.js
@@ -7,7 +7,6 @@ const initialState = {
 
 
 export function searchRobots(state=initialState, action={}) {
-	console.log(action.type)
 	switch(action.type) {
 		case CHANGE_SEARCH_FIELD:
 			return Object.assign({}, state, {searchField: action.payload})
